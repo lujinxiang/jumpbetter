d4910c905c64b858c158c48bc198c00204e46df1
lujinxiang <lujinxiang1009@outlook.com>
2022-12-16 18:41:06 +0800

    day02

--- src/main/java/leetcode/HappyDay01.java
+++ src/main/java/leetcode/HappyDay01.java
@@ -2,9 +2,7 @@ package leetcode;
 
 import common.ListNode;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * LeetCode_Day01
@@ -16,80 +14,113 @@ public class HappyDay01 {
 
     public static void main(String[] args) {
         HappyDay01 happyDay01 = new HappyDay01();
+        int[] arr = new int[]{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4};
+        List<List<Integer>> lists = happyDay01.threeSum(arr);
+        System.out.print(lists);
 
-
-    }
-
-    /**
-     * 1. 两数相加
-     */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<ListNode> stackL1 = new Stack<>();
-        Stack<ListNode> stackL2 = new Stack<>();
-        LinkedList<Integer> queue = new LinkedList<>();
-        while (l1 != null) {
-            stackL1.push(l1);
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            stackL2.push(l2);
-            l2 = l2.next;
-        }
-
-        int next = 0;
-        int cur = 0;
-        while (!stackL1.isEmpty() && !stackL2.isEmpty()) {
-            int sum = stackL1.pop().val + stackL2.pop().val + next;
-            next = sum / 10;
-            cur = sum % 10;
-            queue.addLast(cur);
-        }
-
-        while (!stackL1.isEmpty()) {
-            int sum = stackL1.pop().val + next;
-            next = sum / 10;
-            cur = sum % 10;
-            queue.addLast(cur);
-        }
-
-        while (!stackL2.isEmpty()) {
-            int sum = stackL2.pop().val + next;
-            next = sum / 10;
-            cur = sum % 10;
-            queue.addLast(cur);
-        }
-
-        if (next != 0) {
-            queue.addLast(next);
-        }
-        ListNode newNode = new ListNode(queue.pop());
-        ListNode curNode = newNode;
-        while (!queue.isEmpty()) {
-            ListNode nextNode = new ListNode(queue.pop());
-            curNode.next = nextNode;
-            curNode = newNode;
-        }
-        return newNode;
     }
 
 
     /**
      * 2.无重复字符的最长子串
+     * <p>
+     * <p>
+     * dp[i]表示：i位置上的最长无重复字符子串的长度。
+     * 初始化的时候，dp[i]=1;
      */
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        int[] dp = new int[s.length()];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            dp[i] = 1;
+        }
 
-
-        return 0;
+        for (int i = 1; i < s.length(); i++) {
+            int len = dp[i - 1];
+            if (len == 1) {
+                if (s.charAt(i) != s.charAt(i - 1)) {
+                    dp[i] = len + 1;
+                    max = Math.max(dp[i], max);
+                    continue;
+                }
+            }
+            if (!s.substring(i - len, i).contains(String.valueOf(s.charAt(i)))) {
+                dp[i] = Math.max(dp[i - 1] + 1, dp[i]);
+            } else {
+                for (int j = i - 1; j >= i - len; j--) {
+                    if (s.charAt(j) == s.charAt(i)) {
+                        dp[i] = Math.abs(i - j);
+                        break;
+                    }
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        return max;
     }
 
 
     /**
      * 3.最长回文子串
+     * <p>
+     * str[i...j]是回文子串的情况：
+     * 1.str[i...j]只有一个元素
+     * 2.str[i...j]有两个元素 str[i]==str[j] 是回文串 dp[i][j]=dp[
+     * 3.str[i...j]不只有两个元素，那么str[i]==str[j],且str[i+1...j-1]要是回文子串
+     * <p>
+     * 依赖i+1和j-1位置的值，那么是不是
+     * 1.i方向上需要从下往上遍历
+     * 2.j方向上需要从左往右遍历
+     * <p>
+     * <p>
+     * <p>
+     * dp[i]:表示i位置上的最长回文子串长度为dp[i];
      */
     public String longestPalindrome(String s) {
+        int[][] dpLen = new int[s.length()][s.length()];
+        if (s == null || s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+
+
+        int max = Integer.MIN_VALUE;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = Boolean.TRUE;
+            dpLen[i][i] = 1;
+        }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (Math.abs(i - j) > 1 && dp[i + 1][j - 1]) {
+                        dp[i][j] = Boolean.TRUE;
+                        dpLen[i][j] = Math.max(dpLen[i][j], dpLen[i + 1][j - 1] + 2);
+                    } else if (Math.abs(i - j) <= 1) {
+                        dp[i][j] = Boolean.TRUE;
+                        if (Math.abs(i - j) == 1) {
+                            dpLen[i][j] = Math.max(dpLen[i][j], dpLen[i][i] + 1);
+                        }
+                    }
+                }
+                max = Math.max(1, Math.max(dpLen[i][j], max));
+            }
+        }
+
+
+        for (int i = 0; i < dpLen.length; i++) {
+            for (int j = dpLen.length - 1; j >= i; j--) {
+                if (dpLen[i][j] == max) {
+                    return s.substring(i, j + 1);
+                }
+            }
+        }
         return null;
-
-
     }
 
 
@@ -97,8 +128,44 @@ public class HappyDay01 {
      * 4.Z字形变换
      */
     public String convert(String s, int numRows) {
-        return null;
-
+        if (s == null || s.length() == 0 || numRows <= 1) {
+            return s;
+        }
+        Character[][] dp = new Character[numRows][s.length()];
+        char[] chas = s.toCharArray();
+        boolean rotateFlag = true;
+        int dpColIndex = 0;
+        int chasIndex = 0;
+        int rows = numRows - 1;
+        while (chasIndex < chas.length) {
+            while (rotateFlag) {
+                for (int i = 0; i < numRows; i++) {
+                    if (chasIndex < chas.length) {
+                        dp[i][dpColIndex] = chas[chasIndex++];
+                    }
+                }
+                rotateFlag = false;
+            }
+            if (rows > 1) {
+                for (int i = rows - 1; i > 0; i--) {
+                    if (chasIndex < chas.length) {
+                        dp[i][++dpColIndex] = chas[chasIndex++];
+                    }
+                }
+            }
+            dpColIndex++;
+            rotateFlag = true;
+            rows = numRows - 1;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (dp[i][j] != null) {
+                    sb.append(dp[i][j]);
+                }
+            }
+        }
+        return sb.toString();
     }
 
 
@@ -106,9 +173,28 @@ public class HappyDay01 {
      * 5.整数反转
      */
     public int reverse(int x) {
-        return 0;
+        try {
+            if (x < Integer.MIN_VALUE || x > Integer.MAX_VALUE || x == 0) {
+                return 0;
+            }
+            if (x < 0) {
+                return -1 * reverseNumber(-x);
+            }
+            return reverseNumber(x);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 
-
+    private int reverseNumber(int x) {
+        StringBuilder sb = new StringBuilder();
+        int cur = 0;
+        while (x != 0) {
+            cur = x % 10;
+            x = x / 10;
+            sb.append(cur);
+        }
+        return Integer.valueOf(sb.toString());
     }
 
     /**
@@ -135,8 +221,45 @@ public class HappyDay01 {
     /**
      * 9.三数之和
      */
-    public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> res = new ArrayList<>();
+        if (arr.length < 3) {
+            return res;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        HashMap<Integer, List<Integer>> duplicateMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            left = i + 1;
+            right = arr.length - 1;
+            while (left < right) {
+                int curSum = arr[left] + arr[right];
+                if (curSum == -arr[i]) {
+                    List<Integer> innerList = new ArrayList<>();
+                    if (!duplicateMap.containsKey(arr[i]) || !duplicateMap.get(arr[i]).contains(arr[left])) {
+                        innerList.add(arr[i]);
+                        innerList.add(arr[left]);
+                        innerList.add(arr[right]);
+                        res.add(innerList);
+                        List curList = duplicateMap.get(arr[i]);
+                        if (curList == null) {
+                            curList = new ArrayList();
+                        }
+                        curList.add(arr[left]);
+                        duplicateMap.put(arr[i], curList);
+                    }
+                    left++;
+                    right--;
+                } else if (curSum < -arr[i]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
     }
 
 
--- src/main/java/leetcode/HappyDay02.java
+++ src/main/java/leetcode/HappyDay02.java
@@ -1,4 +1,207 @@
 package leetcode;
 
+import common.ListNode;
+
+/**
+ * @author jinxianglu
+ */
 public class HappyDay02 {
+
+
+    public static void main(String[] args) {
+        HappyDay02 happyDay02 = new HappyDay02();
+        int[] arr = new int[]{5, 7, 7, 8, 8, 10};
+        int[] res = happyDay02.searchRange(arr, 8);
+        System.out.print(res);
+    }
+
+
+    /**
+     * 删除链表倒数第n个节点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || n <= 0) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode low = head;
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        if (n > count) {
+            return head;
+        }
+        //删除头节点
+        if (n == count) {
+            return head.next;
+        }
+        //一般情况，删除的n在合理范围内；
+        for (int i = 0; i < n; i++) {
+            if (fast.next != null) {
+                fast = fast.next;
+            }
+        }
+        while (fast.next != null) {
+            low = low.next;
+            fast = fast.next;
+        }
+        low.next = low.next.next;
+        return head;
+    }
+
+
+    /**
+     * 两两交换链表中的节点
+     *
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode left = head;
+        ListNode right = left.next;
+        ListNode next = right.next;
+        right.next = null;
+        ListNode newHead = reverseNode(left, right);
+        ListNode curHead = newHead;
+        while (next != null && right != null && left != null) {
+            left = next;
+            right = left.next;
+            if (right != null) {
+                next = right.next;
+                right.next = null;
+            } else {
+                next = null;
+            }
+            curHead.next.next = reverseNode(left, right);
+            curHead = curHead.next.next;
+        }
+        return newHead;
+    }
+
+
+    private ListNode reverseNode(ListNode left, ListNode right) {
+        if (right == null) {
+            return left;
+        }
+        right.next = left;
+        left.next = null;
+        return right;
+    }
+
+
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (arr[mid] < target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return arr[low] == target ? low : -1;
+    }
+
+
+    /**
+     * 找出字符串中第一个匹配项的下标
+     *
+     * @param bigStr
+     * @param littleStr
+     * @return
+     */
+    public int strStr(String bigStr, String littleStr) {
+        if (bigStr == null || littleStr == null || bigStr.length() == 0
+                || littleStr.length() == 0 || bigStr.length() < littleStr.length()) {
+            return -1;
+        }
+        int low = 0;
+        int high = 0;
+        while (high < bigStr.length()) {
+            high = low + littleStr.length();
+            if (bigStr.substring(low, high).equals(littleStr)) {
+                return low;
+            }
+            low++;
+        }
+        return -1;
+    }
+
+
+    public int[] searchRange(int[] arr, int target) {
+        int[] res = new int[2];
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        res[0] = -1;
+        res[1] = -1;
+        int left = 0;
+        int right = 0;
+        if (arr == null || arr.length == 0) {
+            return res;
+        }
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                left = search(mid, arr, true);
+                right = search(mid, arr, false);
+                res[0] = left;
+                res[1] = right;
+                return res;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    private int search(int mid, int[] arr, boolean flag) {
+        int res = mid;
+        if (flag) {
+            for (int i = mid; i >= 0; i--) {
+                if (arr[i] != arr[mid]) {
+                    break;
+                }
+                res = i;
+            }
+        } else {
+            for (int i = mid; i < arr.length; i++) {
+                if (arr[i] != arr[mid]) {
+                    break;
+                }
+                res = i;
+            }
+        }
+        return res;
+    }
+
+
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
 }
